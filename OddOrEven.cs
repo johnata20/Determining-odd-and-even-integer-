93377443d449a4be924de5a54795684f8f10ec83
John Rodolfo <114643834+johnata20@users.noreply.github.com>
2022-10-02 00:01:00 -0600

    Add files via upload

---
+++ OddOrEven.cs
@@ -0,0 +1,25 @@
+﻿int inp;
+
+Console.Write("Enter an integer : ");
+inp = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine();
+
+if (inp > 0 && inp % 2 == 0)
+    Console.WriteLine("The input is a positive even number.");
+
+if (inp > 0 && inp % 2 != 0)
+    Console.WriteLine("The input is a positive odd number.");
+
+if (inp < 0 && inp % 2 == 0)
+    Console.WriteLine("The input is a negative even number.");
+
+if (inp < 0 && inp % 2 != 0)
+    Console.WriteLine("The input is a negative odd number.");
+
+if (inp == 0)
+    Console.WriteLine("The input is zero.");
+
+Console.WriteLine();
+Console.WriteLine("Press any key to exit...");
+Console.ReadKey();
+
